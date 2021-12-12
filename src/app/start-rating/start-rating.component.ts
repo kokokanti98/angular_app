49e7a128bba56a9d5bf80e3e0ef502d1fd50e59c
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-12-12 19:44:12 +0100

    Ajout du component enfant star rating

---
+++ src/app/start-rating/start-rating.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-rating',
+  templateUrl: './start-rating.component.html',
+  styleUrls: ['./start-rating.component.css']
+})
+export class StartRatingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
