e269dd62e9c2dcb8a5d95a7fc48ff4dad9d996fd
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-12-12 17:29:14 +0100

    Les interfaces

---
+++ src/app/hotel-list/hotel.ts
@@ -0,0 +1,8 @@
+// le export pour que notre interfacce soit visible
+export interface IHotel{
+    id: number;
+    hotelName: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+}
