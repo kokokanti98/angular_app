1349ecdaaa9762e60baca7a8bb1532852cd8cc97
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-12-18 12:23:18 +0100

    Modification de la structure du fichier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,12 @@ import { HttpClientModule } from '@angular/common/http';
 // Pour utiliser le routage
 import { RouterModule } from '@angular/router';
 // Importer le module pour utiliser Guard
-import { HotelDetailGuard } from './hotel-list/hotel-detail.guard';
+import { HotelDetailGuard } from './hotels/shared/guards/hotel-detail.guard';
 import { AppComponent } from './app.component';
-import { HotelListComponent } from './hotel-list/hotel-list.component';
+import { HotelListComponent } from './hotels//hotel-list/hotel-list.component';
 import { StartRatingComponent } from './start-rating/start-rating.component';
 import { HomeComponent } from './home/home.component';
-import { HotelDetailComponent } from './hotel-list/hotel-detail/hotel-detail.component';
+import { HotelDetailComponent } from './hotels/hotel-detail/hotel-detail.component';
 
 // On va appeller puis en passant par paramètre la langue et son abbréviation dans le code
 registerLocaleData(localeFr, 'fr');
@@ -45,9 +45,9 @@ registerLocaleData(localeFr, 'fr');
       // Redirection vers la liste des hotels quand on accède '/hotels'
       { path: 'hotels', component: HotelListComponent },
       // Redirection vers le detail d'un hotel quand on accède '/hotels/id'
-      { 
+      {
         path: 'hotels/:id', component: HotelDetailComponent,
-        canActivate: [HotelDetailGuard] 
+        canActivate: [HotelDetailGuard]
       },
       // Pour redirection sur les pages en cas de 404 vers home
       { path: '**', redirectTo: 'home', pathMatch: 'full' }
--- src/app/hotel-list/hotel.ts
+++
@@ -1,9 +0,0 @@
-// le export pour que notre interfacce soit visible
-export interface IHotel{
-    hotelId: number;
-    hotelName: string;
-    description: string;
-    price: number;
-    imageUrl: string;
-    rating: number;
-}
