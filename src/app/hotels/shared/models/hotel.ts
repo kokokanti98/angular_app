8aa6aed2ff280af70f4b81a3e64f2a1015d9c1b5
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-12-18 12:22:07 +0100

    Modification de la structure du fichier

---
+++ src/app/hotels/shared/models/hotel.ts
@@ -0,0 +1,9 @@
+// le export pour que notre interfacce soit visible
+export interface IHotel{
+    hotelId: number;
+    hotelName: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    rating: number;
+}
