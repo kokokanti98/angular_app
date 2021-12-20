5e28b51e7907362a7818233da3aa40bdab86b1bc
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-12-20 18:35:12 +0100

    Creation des composants"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
+import { TaskFormComponent } from './task/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule
